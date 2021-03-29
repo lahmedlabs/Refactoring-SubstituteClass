9763e192a47dd2cdee283b427365a19d395c2391
LahmedLabs.com <lahmedlabs@gmail.com>
2021-03-30 00:01:04 +0900

    ItemType class 생성

--- Refactoring-SubstituteClass/Item.h
+++ Refactoring-SubstituteClass/Item.h
@@ -2,16 +2,32 @@
 #include <iostream>
 #include <string>
 
+/*
 #define TYPECODE_BOOK 0
 #define TYPECODE_DVD 1
 #define TYPECODE_SOFTWARE 2
+*/
 /*
 constexpr auto TYPECODE_BOOK = 0;
 constexpr auto TYPECODE_DVD = 1;
 constexpr auto TYPECODE_SOFTWARE = 2;
 */
 using namespace std;
+class ItemType
+{
+private:
+	int _typecode;
+	ItemType(int typecode) : _typecode(typecode) {
+		ItemType BOOK = ItemType(0);
+		ItemType DVD = ItemType(1);
+		ItemType SOFTWARE = ItemType(2);
+	}
 
+public:
+	~ItemType() {}
+	int getTypecode() { return _typecode; }
+
+};
 class Item
 {
 private:
